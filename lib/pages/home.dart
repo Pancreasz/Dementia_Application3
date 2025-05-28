fd0106afb332a0177ef4bfc27377d17969896cbc
jayc <pummasinpp@gmail.com>
2025-05-28 11:33:56 +0700

    test

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,7 +7,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("แบบทดสอบโรคประสาทเสื่อม"),
+        title: const Text("แบบทดสอบโรคประสาทเสื่อม (Test Edition)"),
         automaticallyImplyLeading: false,
       ),
       body: Center(
